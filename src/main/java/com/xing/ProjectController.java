ff29d560ad9554ad850fe829beaf0f69dbdaf75b
honxchen <honxchen@thoughtworks.com>
2020-05-31 13:19:06 +0800

    put items

--- src/main/java/com/xing/ProjectController.java
+++ src/main/java/com/xing/ProjectController.java
@@ -4,15 +4,14 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.DeleteItemResult;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
-import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.model.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -25,8 +24,13 @@ public class ProjectController {
     private String projectTable = "Project_hongxing";
 
     @RequestMapping(value = "/project", method = PUT)
-    public PutItemResult putItem(@RequestBody Map<String, AttributeValue> item) {
-        return client.putItem(projectTable, item);
+    public BatchWriteItemResult putItem(@RequestBody List<Map<String, AttributeValue>> items) {
+        List<WriteRequest> writeRequests = items.stream()
+                .map(item -> new PutRequest(item))
+                .map(putRequest -> new WriteRequest(putRequest))
+                .collect(Collectors.toList());
+        BatchWriteItemRequest request = new BatchWriteItemRequest().addRequestItemsEntry(projectTable, writeRequests);
+        return client.batchWriteItem(request);
     }
 
     @RequestMapping(value = "/project", method = GET)
